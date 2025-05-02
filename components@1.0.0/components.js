d95b5b7af753342c1e0d08e2b507b8c15ab85905
Adam Thomas <thomasadam83@hotmail.com>
2025-05-02 15:29:34 -0400

    Added replace code

--- components@1.0.0/components.js
+++ components@1.0.0/components.js
@@ -12,3 +12,10 @@ for (const [key, html] of Object.entries(components)) {
     console.log(`Component: ${html}`);
 
 }
+
+const replace = document.getElementsByClassName("components")0;
+
+for( let i = 0; i < replace.length; i ++ )
+{
+    console.log(replace[i].getAttribute("id"));
+}
