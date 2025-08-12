d18afeab50a46f2d659325d8727e04d3eeef5607
Adam Thomas <thomasadam83@hotmail.com>
2025-08-12 14:03:34 -0400

    Update components.js

--- components@1.0.0/components.js
+++ components@1.0.0/components.js
@@ -4,14 +4,14 @@ const components = {
         '<hr>' +
         '<h2>BrickMMO Resources</h2>' + 
         '<ul>' +
+        '<li><a href="https://assets.brickmmo.com/">BrickMMO Assets</a></li>' +
         '<li><a href="https://branding.brickmmo.com/">BrickMMO Branding Guidelines</a></li>' +
         '<li><a href="https://cdn.brickmmo.com/">CDN</a></li>' +
-        '<li><a href="https://tasks.brickmmo.com/">Tasks</a></li>' +
         '<li><a href="https://ldr.brickmmo.com/">LDR Directory</a></li>' +
+        '<li><a href="https://tasks.brickmmo.com/">Tasks</a></li>' +
         '</ul>' + 
         '<h2>CodeAdam Resources</h2>' +
         '<ul>' + 
-        '<li><a href="https://activities.codeadam.ca">Team Building Activities</a></li>' +
         '<li><a href="https://branding.codeadam.ca/">CodeAdam Branding Guidelines</a></li>' +
         '<li><a href="https://cdn.codeadam.ca/">CDN</a></li>' +
         '<li><a href="https://challenges.codeadam.ca/">Coding Challenges</a></li>' +
@@ -19,6 +19,7 @@ const components = {
         '<li><a href="https://pseudocode.codeadam.ca/">Pseudocode Guidelines</a></li>' +
         '<li><a href="https://questions.codeadam.ca/">Interview Questions</a></li>' +
         '<li><a href="https://readme.codeadam.ca/">_readme</a></li>' +
+        '<li><a href="https://activities.codeadam.ca">Team Building Activities</a></li>' +
         '<li><a href="https://tidy.codeadam.ca/">Tidy</a></li>' +
         '</ul>'
 };
